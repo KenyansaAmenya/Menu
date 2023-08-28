c7252f2318f5f7fec9779ed273355ba71d08ec86
KenyansaAmenya <100693771+KenyansaAmenya@users.noreply.github.com>
2023-08-28 08:07:04 +0300

    My Recipe Menu

---
+++ script.js
@@ -0,0 +1,14 @@
+const recipes = [
+    { title: "Chocolate Chip Cookies", description: "Classic homemade chocolate chip cookies."},
+    // add more recipes here
+];
+
+const recipeList = document.querySelector(".recipe-list");
+const recipeDetails = document.querySelector(".recipe-details");
+const recipeTitle = document.querySelector("#recipe-title");
+const recipeDescription = document.querySelector("#recipe-description");
+const closeDetailsButton = document.querySelector("#close-details");
+
+recipes.forEach(recipe => {
+    
+})
